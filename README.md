c8d85c060fe7bcaf510a88ee5461e6d884d6e44d
bn-pam <bonneau.pam@gmail.com>
2024-12-05 12:08:56 +0100

    bugfix : copytodatalake + factorisation du code

--- README.md
+++ README.md
@@ -7,13 +7,15 @@ it copies it in a variables.tfvars file in the same module
 ## copytodatalake
 consists in a file-copier to a specific datalake
 needs a .env file with following attributes : 
-SP_ID_SECONDARY = ""
-SP_SECONDARY_PASSWORD = ""
-SP_ID_PRINCIPAL = ""
-TENANT_ID = ""
-KEYVAULT_URL = ""
-SECRET_NAME = ""
-STORAGE_ACCOUNT_NAME = ""
+SP_ID_SECONDARY=""
+SP_SECONDARY_PASSWORD=""
+SP_ID_PRINCIPAL=""
+TENANT_ID=""
+KEYVAULT_URL=""
+SECRET_NAME=""
+STORAGE_ACCOUNT_NAME=""
+CONTAINER_NAME=""
+FILE_PATH=""
 
 ## keyvault (and keyvault_secret)
 consists in a keyvault secret generator
--- copytodatalake/script_copy.py
+++ copytodatalake/script_copy.py
@@ -3,7 +3,7 @@ from azure.storage.filedatalake import DataLakeServiceClient
 from azure.identity import ClientSecretCredential
 from azure.keyvault.secrets import SecretClient
 from dotenv import load_dotenv
-import uuid
+import uuid, requests
 
 # Charger les variables d'environnement depuis le fichier .env
 load_dotenv()
@@ -15,10 +15,11 @@ tenant_id = os.getenv('TENANT_ID')
 vault_url = os.getenv('KEYVAULT_URL')
 storage_account_name = os.getenv('STORAGE_ACCOUNT_NAME')
 sp1_id = os.getenv('SP_ID_PRINCIPAL')
+sp1_secret = os.getenv('SECRET_NAME')
 
 # Récupérer les informations du fichier à uploader
-local_file_path = "path/to/your/file.csv"
-file_system_name = "your-file-system-name"
+upload_file_path = os.getenv('FILE_PATH')
+file_system_name = os.getenv('CONTAINER_NAME')
 file_name = f"{file_system_name}_{uuid.uuid4().hex}.csv"  # Nom dynamique pour éviter les collisions
 
 # Récupérer les secrets stockés dans Azure Key Vault pour le Service Principal 1
@@ -31,7 +32,7 @@ sp2_credential = ClientSecretCredential(tenant_id, sp2_id, sp2_secret)
 secret_client = SecretClient(vault_url=vault_url, credential=sp2_credential)
 
 # interroger le keyvault pour donner le secret du Service Principal 1 : get_secret obtient les infos (value, name, id et properties, et .value retourne juste la valeur du secret)
-sp1_secret = secret_client.get_secret(sp1_id).value
+sp1_secret = secret_client.get_secret(sp1_secret).value
 
 # Créer un client pour le Service Principal 1
 sp1_credential = ClientSecretCredential(tenant_id, sp1_id, sp1_secret)
@@ -44,18 +45,33 @@ def get_datalake_client():
     )
     return service_client_datalake
 
+
+# fonction de récupération de fichier depuis request
+def get_response_url() :
+    url = upload_file_path
+    # Télécharger le fichier depuis l'URL
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.content
+        print("Fichier téléchargé avec succès depuis l'URL.")
+    else:
+        print(f"Échec du téléchargement, code de statut: {response.status_code}")
+        exit(1)
+
 # fonction permettant d'uploader le fichier sur le datalake
-def upload_file_to_datalake(local_file_path, file_system_name, file_name):
-    datalake_client = get_datalake_client()
+def upload_file_to_datalake(file_content, file_system_name, file_name):
+
     file_system_client = datalake_client.get_file_system_client(file_system_name)
     
-    # Créer ou obtenir un fichier dans le DataLake
+    # Copier un fichier dans le DataLake
     file_client = file_system_client.create_file(file_name)
-    
-    with open(local_file_path, "rb") as local_file:
-        file_client.append_data(local_file.read(), 0)
-        file_client.flush_data(local_file.tell())
+    file_client.append_data(file_content, 0)
+    file_client.flush_data(len(file_content)) #taille totale du contenu du fichier (en bytes). 
+    #Cela est important car Azure Data Lake utilise cette information pour s'assurer que toutes les données ont bien été envoyées et que la session d'écriture est terminée.
+
     print(f"File '{file_name}' uploaded successfully to Data Lake.")
 
-# appeler ma fonction
-upload_file_to_datalake(local_file_path, file_system_name, file_name)
+# appeler mes fonctions pour rendre le fonctionnement plus digeste
+file_content = get_response_url()
+datalake_client = get_datalake_client()
+upload_file_to_datalake(file_content, file_system_name, file_name)
